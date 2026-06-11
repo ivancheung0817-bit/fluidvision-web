730949d39876429b384927f744c2b2e730278a67
Wang Hetong <wanghetong@proton.me>
2026-06-11 16:35:41 +0800

    general fix

--- controller/js/compiled/cljs-runtime/shadow.module.main.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('main');
-
-try { pumpctrl.app.init(); } catch (e) { console.error("An error occurred when calling (pumpctrl.app/init)"); console.error(e); }
